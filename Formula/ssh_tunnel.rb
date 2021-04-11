95c5e14919856fc7d3fbbc3012a5ed1596621cbc
Bruno Murino <bfsmurino@gmail.com>
2021-04-11 20:19:10 +0100

    updated formula

--- Formula/ssh_tunnel.rb
+++ Formula/ssh_tunnel.rb
@@ -1,9 +1,9 @@
 class SshTunnel < Formula
     desc "Manager of SSH Tunnels written in Rust"
     homepage "https://github.com/brunomurino/ssh_tunnel"
-    url "https://github.com/brunomurino/ssh_tunnel/releases/download/v0.1.0/ssh_tunnel-mac.tar.gz"
-    sha256 "62688fe1e3336667e5fb842076a06fdabd805966a7300dd59bc9e4e52be0a1a4"
-    version "0.1.0"
+    url "https://github.com/brunomurino/ssh_tunnel/releases/download/v0.0.3/ssh_tunnel-mac.tar.gz"
+    sha256 "34e70e819162bb698a189fde347368854ce91fa196e9accef52f249be890e3c2"
+    version "0.0.3"
   
     def install
       bin.install "ssh_tunnel"
